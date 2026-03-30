7844223e553cdec4039cb78191cf79e570d95eab
anushka130126 <Gmail>
2026-03-31 01:25:05 +0530

    Fixed missing auth urls and template naming

--- clinic_core/urls.py
+++ clinic_core/urls.py
@@ -1,9 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('clinic-hq-vault-88/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')), 
     path('', include('appointments.urls')), 
 ]
-
